8704e424677b3125990ccd55e252ed4178d83fba
Samantha <samanthavillafana8@gmail.com>
2023-12-17 14:12:53 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    buildToDo(event.target.new_task_description.value);
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(event){
+  event.target.parentNode.remove()
+}
